af26fdc1d83153efe757a1f06d5e69c6b0f02ab0
sammy khaleel <sammykhaleel83@gmail.com>
2019-04-03 15:04:24 -0400

    roll back

--- src/components/PickImage/PickImage.js
+++ src/components/PickImage/PickImage.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
 import { View, Image, Button, StyleSheet } from "react-native";
-
-import imagePlaceholder from "../../assets/beautiful-place.jpg";
 import ImagePicker from "react-native-image-picker";
-// import console = require("console");
 
 class PickImage extends Component {
-
   state = {
     pickedImaged: null
   }
@@ -14,22 +10,23 @@ class PickImage extends Component {
   pickImageHandler = () => {
     ImagePicker.showImagePicker({title: "Pick an Image"}, res => {
       if (res.didCancel) {
-        console.log("User cancelled");
+        console.log("User cancelled!");
       } else if (res.error) {
         console.log("Error", res.error);
       } else {
         this.setState({
-          pickdImaged: {uri: res.uri}
+          pickedImaged: { uri: res.uri }
         });
-        this.props.onImagePicked({uri: res.uri, based64: res.data});
+        this.props.onImagePicked({uri: res.uri, base64: res.data});
       }
     });
   }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.placeholder}>
-          <Image source={this.pickedImaged} style={styles.previewImage} />
+          <Image source={this.state.pickedImaged} style={styles.previewImage} />
         </View>
         <View style={styles.button}>
           <Button title="Pick Image" onPress={this.pickImageHandler} />
